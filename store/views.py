12a859b6391efef55bb1001b134c59c5e4076dfe
odiegofarias <diego.farias.fernandes@gmail.com>
2022-12-08 19:17:53 -0300

    Criados templates e urls e feitas as devidas conexções

--- store/views.py
+++ store/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def store(request):
+    template = 'store/store.html'
+    context = {}
+    return render(request, template, context)
+
+def cart(request):
+    template = 'store/cart.html'
+    context = {}
+    return render(request, template, context)
+
+def checkout(request):
+    template = 'store/checkout.html'
+    context = {}
+    return render(request, template, context)
